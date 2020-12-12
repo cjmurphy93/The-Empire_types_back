f49c2be3c852dcb8890c305e1be05e5b1346e1ef
cjmurphy93 <cjmurph93@gmail.com>
2020-12-11 22:13:56 -0500

    cleaning up

--- src/index.js
+++ src/index.js
@@ -1,79 +1,73 @@
 import "./styles/index.scss";
-import Game from './scripts/game';
-import * as firebaseAPI from './scripts/firebasedb';
+import Game from "./scripts/game";
+import * as firebaseAPI from "./scripts/firebasedb";
 
+document.addEventListener("DOMContentLoaded", () => {
+  // firebaseAPI.signIn();
+  const canvas = document.getElementById("ship-game");
+  const ctx = canvas.getContext("2d");
+  const bgCanvas = document.getElementById("bg-canvas");
+  const bgCtx = bgCanvas.getContext("2d");
+  const pCanvas = document.getElementById("player-screen");
+  const pCtx = pCanvas.getContext("2d");
 
+  const inputSection = document.getElementById("input-section");
 
-document.addEventListener('DOMContentLoaded', () => {
-    // firebaseAPI.signIn();
-    const canvas = document.getElementById("ship-game");
-    const ctx = canvas.getContext('2d');
-    const bgCanvas = document.getElementById("bg-canvas");
-    const bgCtx = bgCanvas.getContext('2d');
-    const pCanvas = document.getElementById("player-screen");
-    const pCtx = pCanvas.getContext('2d');
+  const playerWord = document.getElementById("player-word");
+  playerWord.autofocus = true;
 
-    const inputSection = document.getElementById("input-section");
-    
-    const playerWord = document.getElementById('player-word');
-    playerWord.autofocus = true;
+  inputSection.addEventListener("submit", handleSubmit);
+  var newGame = new Game(canvas, ctx, bgCtx, pCtx);
 
-    inputSection.addEventListener('submit', handleSubmit);
-    var newGame = new Game(canvas, ctx, bgCtx, pCtx);
-    
-    document.addEventListener("keypress", removeSplash);
+  document.addEventListener("keypress", removeSplash);
 
-    const startBtn = document.getElementById('start');
-    startBtn.addEventListener("click", removeSplash);
+  const startBtn = document.getElementById("start");
+  startBtn.addEventListener("click", removeSplash);
 
-    document.addEventListener('keydown', restartGame)
+  document.addEventListener("keydown", restartGame);
 
-    
-
-    function removeSplash(e) {
-        if ((e.key === 'Enter') || (e.type === "click")) {
-            const splash = document.getElementById("splash");
-            const leaderboard = document.getElementById("leaderboard-container");
-            leaderboard.style.display = 'none';
-            splash.style.display = 'none';
-            document.removeEventListener("keypress", removeSplash);
-            startBtn.removeEventListener("click", removeSplash);
-            newGame.start();
-            playerWord.focus();
-        }
+  function removeSplash(e) {
+    if (e.key === "Enter" || e.type === "click") {
+      const splash = document.getElementById("splash");
+      const leaderboard = document.getElementById("leaderboard-container");
+      leaderboard.style.display = "none";
+      splash.style.display = "none";
+      document.removeEventListener("keypress", removeSplash);
+      startBtn.removeEventListener("click", removeSplash);
+      newGame.start();
+      playerWord.focus();
     }
+  }
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    newGame.checkWord(playerWord.value.toLowerCase().trim());
+    inputSection.reset();
+  }
 
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        newGame.checkWord(playerWord.value.toLowerCase().trim());
+  function restartGame(e) {
+    if (newGame.gameOver) {
+      if (e.ctrlKey && (e.key === " " || e.key === "Spacebar")) {
+        const leaderboard = document.getElementById("leaderboard-container");
+        leaderboard.style.display = "none";
+        newGame = null;
+        newGame = new Game(canvas, ctx, bgCtx, pCtx);
+        const playerWord = document.getElementById("player-word");
+        playerWord.autoFocus = true;
+        const inputSection = document.getElementById("input-section");
+        inputSection.style.display = "block";
         inputSection.reset();
-    };
-
-    function restartGame(e) {
-        if (newGame.gameOver) {
-            if (e.ctrlKey && (e.key === ' ' || e.key === 'Spacebar')) {
-            const leaderboard = document.getElementById("leaderboard-container");
-            leaderboard.style.display = 'none';
-                newGame = null;    
-                newGame = new Game(canvas, ctx, bgCtx, pCtx);
-                const playerWord = document.getElementById('player-word');
-                playerWord.autoFocus = true;
-                const inputSection = document.getElementById("input-section");
-                inputSection.style.display = 'block';
-                inputSection.reset();
-                newGame.start();
-                playerWord.focus();
-            }
-        }
+        newGame.start();
+        playerWord.focus();
+      }
     }
+  }
 
-    // const body = document.getElementsByTagName('BODY')
-    // body.addEventListener('beforeunload', firebase.signOut)
-    // body.addEventListener('unload', e => {
-    //         firebaseAPI.signOut();
-    //     });
-    });
+  // const body = document.getElementsByTagName('BODY')
+  // body.addEventListener('beforeunload', firebase.signOut)
+  // body.addEventListener('unload', e => {
+  //         firebaseAPI.signOut();
+  //     });
+});
 
-    // window.onunload = firebaseAPI.signOut
+// window.onunload = firebaseAPI.signOut
--- src/scripts/player.js
+++ src/scripts/player.js
@@ -11,29 +11,177 @@ export default class Player {
     this.score = 0;
   }
 
-
   animate() {
-        this.ctx.drawImage(this.pImg, 2, 450, 256, 163, 0, ((59 / 222) * this.canvas.height), this.canvas.width, ((163 / 222) * this.canvas.height));
-        this.ctx.drawImage(this.pImg, this.navScreenFrames[this.nSF], 580, 54, 33, ((101 / 256) * this.canvas.width), ((176 / 222) * this.canvas.height), ((54 / 256) * this.canvas.width), ((33 / 222) * this.canvas.height));
-        this.nSF += 1;
-        if (this.nSF > 1) this.nSF = 0;
+    this.ctx.drawImage(
+      this.pImg,
+      2,
+      450,
+      256,
+      163,
+      0,
+      (59 / 222) * this.canvas.height,
+      this.canvas.width,
+      (163 / 222) * this.canvas.height
+    );
+    this.ctx.drawImage(
+      this.pImg,
+      this.navScreenFrames[this.nSF],
+      580,
+      54,
+      33,
+      (101 / 256) * this.canvas.width,
+      (176 / 222) * this.canvas.height,
+      (54 / 256) * this.canvas.width,
+      (33 / 222) * this.canvas.height
+    );
+    this.nSF += 1;
+    if (this.nSF > 1) this.nSF = 0;
 
-        if ( this.health <= 9 ) this.ctx.drawImage(this.pImg, 268, 561, 6, 3, ((161 / 256) * this.canvas.width), ((153 / 222) * this.canvas.height), ((6 / 256) * this.canvas.width), ((3 / 222) * this.canvas.height));
-        if ( this.health <= 8 ) this.ctx.drawImage(this.pImg, 268, 561, 6, 3, ((153 / 256) * this.canvas.width), ((153 / 222) * this.canvas.height), ((6 / 256) * this.canvas.width), ((3 / 222) * this.canvas.height));
-        if ( this.health <= 7 ) this.ctx.drawImage(this.pImg, 268, 561, 6, 3, ((145 / 256) * this.canvas.width), ((153 / 222) * this.canvas.height), ((6 / 256) * this.canvas.width), ((3 / 222) * this.canvas.height));
-        if ( this.health <= 6 ) this.ctx.drawImage(this.pImg, 268, 561, 6, 3, ((137 / 256) * this.canvas.width), ((153 / 222) * this.canvas.height), ((6 / 256) * this.canvas.width), ((3 / 222) * this.canvas.height));
-        if ( this.health <= 5 ) this.ctx.drawImage(this.pImg, 268, 561, 6, 3, ((129 / 256) * this.canvas.width), ((153 / 222) * this.canvas.height), ((6 / 256) * this.canvas.width), ((3 / 222) * this.canvas.height));
-        if ( this.health <= 4 ) this.ctx.drawImage(this.pImg, 268, 561, 6, 3, ((121 / 256) * this.canvas.width), ((153 / 222) * this.canvas.height), ((6 / 256) * this.canvas.width), ((3 / 222) * this.canvas.height));
-        if ( this.health <= 3 ) this.ctx.drawImage(this.pImg, 268, 561, 6, 3, ((113 / 256) * this.canvas.width), ((153 / 222) * this.canvas.height), ((6 / 256) * this.canvas.width), ((3 / 222) * this.canvas.height));
-        if ( this.health <= 2 ) this.ctx.drawImage(this.pImg, 268, 561, 6, 3, ((105 / 256) * this.canvas.width), ((153 / 222) * this.canvas.height), ((6 / 256) * this.canvas.width), ((3 / 222) * this.canvas.height));
-        if ( this.health <= 1 ) this.ctx.drawImage(this.pImg, 268, 561, 6, 3, ((97 / 256)  * this.canvas.width), ((153 / 222) * this.canvas.height), ((6 / 256 ) * this.canvas.width), ((3 / 222) * this.canvas.height));
-        if ( this.health <= 0 ) this.ctx.drawImage(this.pImg, 268, 561, 6, 3, ((89 / 256) * this.canvas.width), ((153 / 222) * this.canvas.height), ((6 / 256) * this.canvas.width), ((3 / 222) * this.canvas.height));
+    if (this.health <= 9)
+      this.ctx.drawImage(
+        this.pImg,
+        268,
+        561,
+        6,
+        3,
+        (161 / 256) * this.canvas.width,
+        (153 / 222) * this.canvas.height,
+        (6 / 256) * this.canvas.width,
+        (3 / 222) * this.canvas.height
+      );
+    if (this.health <= 8)
+      this.ctx.drawImage(
+        this.pImg,
+        268,
+        561,
+        6,
+        3,
+        (153 / 256) * this.canvas.width,
+        (153 / 222) * this.canvas.height,
+        (6 / 256) * this.canvas.width,
+        (3 / 222) * this.canvas.height
+      );
+    if (this.health <= 7)
+      this.ctx.drawImage(
+        this.pImg,
+        268,
+        561,
+        6,
+        3,
+        (145 / 256) * this.canvas.width,
+        (153 / 222) * this.canvas.height,
+        (6 / 256) * this.canvas.width,
+        (3 / 222) * this.canvas.height
+      );
+    if (this.health <= 6)
+      this.ctx.drawImage(
+        this.pImg,
+        268,
+        561,
+        6,
+        3,
+        (137 / 256) * this.canvas.width,
+        (153 / 222) * this.canvas.height,
+        (6 / 256) * this.canvas.width,
+        (3 / 222) * this.canvas.height
+      );
+    if (this.health <= 5)
+      this.ctx.drawImage(
+        this.pImg,
+        268,
+        561,
+        6,
+        3,
+        (129 / 256) * this.canvas.width,
+        (153 / 222) * this.canvas.height,
+        (6 / 256) * this.canvas.width,
+        (3 / 222) * this.canvas.height
+      );
+    if (this.health <= 4)
+      this.ctx.drawImage(
+        this.pImg,
+        268,
+        561,
+        6,
+        3,
+        (121 / 256) * this.canvas.width,
+        (153 / 222) * this.canvas.height,
+        (6 / 256) * this.canvas.width,
+        (3 / 222) * this.canvas.height
+      );
+    if (this.health <= 3)
+      this.ctx.drawImage(
+        this.pImg,
+        268,
+        561,
+        6,
+        3,
+        (113 / 256) * this.canvas.width,
+        (153 / 222) * this.canvas.height,
+        (6 / 256) * this.canvas.width,
+        (3 / 222) * this.canvas.height
+      );
+    if (this.health <= 2)
+      this.ctx.drawImage(
+        this.pImg,
+        268,
+        561,
+        6,
+        3,
+        (105 / 256) * this.canvas.width,
+        (153 / 222) * this.canvas.height,
+        (6 / 256) * this.canvas.width,
+        (3 / 222) * this.canvas.height
+      );
+    if (this.health <= 1)
+      this.ctx.drawImage(
+        this.pImg,
+        268,
+        561,
+        6,
+        3,
+        (97 / 256) * this.canvas.width,
+        (153 / 222) * this.canvas.height,
+        (6 / 256) * this.canvas.width,
+        (3 / 222) * this.canvas.height
+      );
+    if (this.health <= 0)
+      this.ctx.drawImage(
+        this.pImg,
+        268,
+        561,
+        6,
+        3,
+        (89 / 256) * this.canvas.width,
+        (153 / 222) * this.canvas.height,
+        (6 / 256) * this.canvas.width,
+        (3 / 222) * this.canvas.height
+      );
 
-      let tens = Math.floor(this.score / 10);
-      let ones = this.score % 10;
-      
-      this.ctx.drawImage(this.pImg, this.scoreFrames[tens], 571, 6, 7, ((169 / 256) * this.canvas.width), ((200 / 222) * this.canvas.height), ((6 / 256) * this.canvas.width), ((7 / 222) * this.canvas.height));
-      this.ctx.drawImage(this.pImg, this.scoreFrames[ones], 571, 6, 7, ((177 / 256) * this.canvas.width), ((200 / 222) * this.canvas.height), ((6 / 256) * this.canvas.width), ((7 / 222) * this.canvas.height));
-      
+    let tens = Math.floor(this.score / 10);
+    let ones = this.score % 10;
+
+    this.ctx.drawImage(
+      this.pImg,
+      this.scoreFrames[tens],
+      571,
+      6,
+      7,
+      (169 / 256) * this.canvas.width,
+      (200 / 222) * this.canvas.height,
+      (6 / 256) * this.canvas.width,
+      (7 / 222) * this.canvas.height
+    );
+    this.ctx.drawImage(
+      this.pImg,
+      this.scoreFrames[ones],
+      571,
+      6,
+      7,
+      (177 / 256) * this.canvas.width,
+      (200 / 222) * this.canvas.height,
+      (6 / 256) * this.canvas.width,
+      (7 / 222) * this.canvas.height
+    );
   }
 }
