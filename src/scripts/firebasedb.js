1fd2fc6beebc4284dacf546430c1555739d16deb
cjmurphy93 <cjmurph93@gmail.com>
2020-12-18 23:07:19 -0500

    cleaning

--- src/scripts/firebasedb.js
+++ src/scripts/firebasedb.js
@@ -3,66 +3,70 @@ import "firebase/firestore";
 import "firebase/auth";
 
 firebase.initializeApp({
-    apiKey: "AIzaSyCwgZPZIAN-oBIcYnxU8cdoB07zheYEkek",
-    authDomain: "the-empire-types-back.firebaseapp.com",
-    projectId: "the-empire-types-back",
+  apiKey: "AIzaSyCwgZPZIAN-oBIcYnxU8cdoB07zheYEkek",
+  authDomain: "the-empire-types-back.firebaseapp.com",
+  projectId: "the-empire-types-back",
 });
 
-     
 export function signIn() {
-    firebase.auth().signInAnonymously();            
+  firebase.auth().signInAnonymously();
 }
 
-        
 var db = firebase.firestore();
-
 
 var scores = [];
 
-export function getScores(){
-    scores = [];
-    var user = firebase.auth().currentUser;
-    if (!user) signIn();
-    const fetchedScores = db.collection('hiscores').orderBy('score', 'desc').limit(10).get();
-    fetchedScores.then(snapshot => {
-        snapshot.docs.forEach( entry => {
-            scores.push( entry.data().score)
-        })
-    })
-    return fetchedScores;
-};
-
-export function checkScore(score){
-
-    let outcome = false;
-    scores.forEach(hiscore => {
-        if (score> hiscore) outcome = true
-    })
-
-    return outcome
+export function getScores() {
+  scores = [];
+  var user = firebase.auth().currentUser;
+  if (!user) signIn();
+  const fetchedScores = db
+    .collection("hiscores")
+    .orderBy("score", "desc")
+    .limit(10)
+    .get();
+  fetchedScores.then((snapshot) => {
+    snapshot.docs.forEach((entry) => {
+      scores.push(entry.data().score);
+    });
+  });
+  return fetchedScores;
 }
 
+export function checkScore(score) {
+  let outcome = false;
+  scores.forEach((hiscore) => {
+    if (score > hiscore) outcome = true;
+  });
 
-export function addScore(name, score){
-   var d = new Date();
-   var n = d.toDateString();
-   return db.collection('hiscores').add({
-        name: name,
-        score: score,
-        date: n
+  return outcome;
+}
+
+export function addScore(name, score) {
+  var d = new Date();
+  var n = d.toDateString();
+  return db
+    .collection("hiscores")
+    .add({
+      name: name,
+      score: score,
+      date: n,
     })
-    .then( () => true)
-    .catch( () => false )
+    .then(() => true)
+    .catch(() => false);
 }
 
 export function signOut() {
-    firebase.auth().signOut().then( () => {
-        db.collection('hiscores').add({
-        name: OUT,
-        score: 0
-    })
-    .then( () => true)
-    .catch( () => false );
-    }
-);
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      db.collection("hiscores")
+        .add({
+          name: OUT,
+          score: 0,
+        })
+        .then(() => true)
+        .catch(() => false);
+    });
 }
